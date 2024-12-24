c28840991a52bf270e2542713831458792552ae4
Crimson <jamison.a.loy@gmail.com>
2024-12-24 12:12:26 -0600

    Added linux build support and gitignore

--- build/client/CMakeFiles/client.dir/client.cpp.o.d
+++
@@ -1,4 +0,0 @@
-client/CMakeFiles/client.dir/client.cpp.o: \
- /home/crimson/Public/KoboldKhat/KoboldKhat/client/client.cpp \
- /usr/include/stdc-predef.h \
- /home/crimson/Public/KoboldKhat/KoboldKhat/client/client.h
--- build/server/CMakeFiles/server.dir/server.cpp.o.d
+++
@@ -1,4 +0,0 @@
-server/CMakeFiles/server.dir/server.cpp.o: \
- /home/crimson/Public/KoboldKhat/KoboldKhat/server/server.cpp \
- /usr/include/stdc-predef.h \
- /home/crimson/Public/KoboldKhat/KoboldKhat/server/server.h
